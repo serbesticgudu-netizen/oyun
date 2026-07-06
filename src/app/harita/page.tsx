871c9cfa56c043e8820469cef541f3b9e19ece84
serbesticgudu-netizen <serbesticgudu@gmail.com>
2026-07-07 01:46:01 +0300

    guncel

--- src/app/harita/page.tsx
+++ src/app/harita/page.tsx
@@ -532,8 +532,8 @@ leafletRef.current = map
   >
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
-      width="20" 
-      height="20" 
+      width="25" 
+      height="25" 
       viewBox="0 0 24 24" 
       fill="none" 
       stroke="currentColor" 
